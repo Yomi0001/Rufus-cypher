90f811b4977887b0747299950fbbb91fb0c488bb
Rufus <57686633+Yomi0001@users.noreply.github.com>
2020-03-27 11:48:03 +0100

    Add files via upload

---
+++ rufus.js
@@ -0,0 +1,9 @@
+var name = "yomi"
+var courses = ["HTML", "CSS", "and", "Javascript"]
+const data = [];
+for (var i = 1; i < 200; i += 2) {
+    data.push(i)
+}
+console.log ("my name is " + name);
+console.log ("I offer " + courses);
+console.log (data);
